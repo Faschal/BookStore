d138e715aae5b0d3c74f63e2fed4087aa6d4a357
Faschal <faschal21@gmail.com>
2020-12-15 13:13:52 +0700

    Add book controller

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string getAllBooks()
+        {
+            return "All books";
+        }
+
+        public string getBook(int id)
+        {
+            return $"book with id = {id}";
+        }
+
+        public string searchBooks(string name, string author)
+        {
+            return $"Book with name = {name} and author = {author}";
+        }
+    }
+}
